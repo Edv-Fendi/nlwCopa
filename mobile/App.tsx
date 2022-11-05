f02e00ac9064f57bd989d5d27df42459a1c0ffe1
Edvaldo de Fendi <edvaldo.defendi@gmail.com>
2022-11-05 20:59:30 -0300

    fix

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,8 @@
 import { NativeBaseProvider, StatusBar } from "native-base";
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from "@expo-google-fonts/roboto";
 
-import { Pools } from './src/screens/Pools';
+
+import { SignIn } from './src/screens/SignIn';
 import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme'
@@ -19,7 +20,7 @@ export default function App() {
           barStyle="light-content" backgroundColor="transparent" translucent
         />
         {
-          fontsLoaded ? <Pools /> : <Loading />
+          fontsLoaded ? <SignIn /> : <Loading />
         }
       </AuthContextProvider>
     </NativeBaseProvider>
