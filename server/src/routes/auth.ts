e5a45155e7f9735855f2678b1aab20e6617fa7ee
Edvaldo de Fendi <edvaldo.defendi@gmail.com>
2022-11-19 20:12:31 -0300

    authenticate

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from "fastify"
 import { z } from "zod"
 import { prisma } from "../lib/prisma"
-import { authentication } from "../plugins/authenticate"
+import { authenticate } from "../plugins/authenticate"
 
 
 
@@ -9,7 +9,7 @@ export async function authRoutes(fastify: FastifyInstance){
   fastify.get(
     '/me', 
     {
-      onRequest: [authentication],
+      onRequest: [authenticate],
     }, 
   async(request) => {
   
